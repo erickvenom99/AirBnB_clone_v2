b305ebdd9be9fd8e4ea7d77eaa606a3ba03ebe88
ericvenom99 <prevenakhtar@mail.com>
2024-07-26 20:25:05 +0200

    task 1

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+Module define a flask app
+"""
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """prints "Hello HBNB!" on the browser
+    """
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb')
+def hbnb():
+    """display "HBNB" on the br
+    """
+    return "HBNB"
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=5000)
